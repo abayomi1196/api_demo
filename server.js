236ba9210218e2a15efd0facb30fef57244e488e
Abayomi Ayodele <samabayomi301@gmail.com>
2022-06-24 06:34:02 +0100

    initital express routing

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ dotenv.config({ path: "./config/config.env" });
 // init express
 const app = express();
 
+// routes
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, message: "Show all bootcamps" });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, message: "Create new  bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Show single bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 8888;
 app.listen(
   PORT,
