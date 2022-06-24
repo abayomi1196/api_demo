f763aebcb85c919d71100c207b56bfb53119796e
Abayomi Ayodele <samabayomi301@gmail.com>
2022-06-24 20:47:50 +0100

    connect to mongodb using mongoose

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
-// route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// connect to mongo db
+connectDB();
+
+// route files
+const bootcamps = require("./routes/bootcamps");
 
 // init express
 const app = express();
@@ -20,7 +25,15 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 8888;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
 );
+
+// handle all unhandled rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // close server & exit process
+  server.close(() => process.exit(1));
+});
