abbdfaad1dc67de6b95eb07131ac69a882be2205
Abayomi Ayodele <samabayomi301@gmail.com>
2022-06-24 20:52:08 +0100

    setup `colors` for intuitive logs

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log(`MongoDB connected: ${conn.connection.host}`);
+  console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 
 const connectDB = require("./config/db");
 
@@ -27,12 +28,15 @@ app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 8888;
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow
+      .bold
+  )
 );
 
 // handle all unhandled rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
 
   // close server & exit process
   server.close(() => process.exit(1));
