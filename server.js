93df56041ad714f0f7e09b1207704168716751d0
Abayomi Ayodele <samabayomi301@gmail.com>
2022-06-24 06:40:00 +0100

    file based routing

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// route files
+const bootcamps = require("./routes/bootcamps");
+
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
@@ -8,31 +11,7 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 // routes
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, message: "Show all bootcamps" });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, message: "Create new  bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Show single bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
-});
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 8888;
 app.listen(
