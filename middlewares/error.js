1d6439f033a0b0d5cc3e6d2db142bcf7c1d95e1d
Abayomi Ayodele <samabayomi301@gmail.com>
2022-06-26 21:31:19 +0100

    extend custom error validation

--- middlewares/error.js
+++ middlewares/error.js
@@ -5,12 +5,28 @@ const errorHandler = (err, req, res, next) => {
   let customError = { ...err };
 
   // log error to console
-  console.log(err.stack.red);
+  console.log({
+    err,
+    errName: err.name,
+    isErrArray: Array.isArray(err.errors)
+  });
 
   // mongoose bad object id err name is `CastError`
   if (err.name === "CastError") {
     const message = `Resource not found with id: ${err.value}`;
     customError = new ErrorResponse(message, 404);
+  }
+
+  // mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value: '${err.keyValue.name}' entered`;
+    customError = new ErrorResponse(message, 400);
+  }
+
+  // mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    customError = new ErrorResponse(message, 400);
   }
 
   res
